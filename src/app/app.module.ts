6f3f53eb92fb34da5e8d86a93103902113fb7519
utsavmaniyar <utsavmaniyar@gmail.com>
2017-03-19 02:09:08 -0700

    chart added for data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,21 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { DeviceDataComponent } from './component/device-data/device-data.component';
+import { OrderByPipe } from './pipes/order-by.pipe';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DeviceDataComponent
+    DeviceDataComponent,
+    OrderByPipe,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpModule
+    HttpModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/device-data/device-data.component.ts
+++ src/app/component/device-data/device-data.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DeviceDataService } from '../../service/device-data.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { OrderByPipe } from '../../pipes/order-by.pipe';
 
 
 @Component({
@@ -17,9 +18,49 @@ export class DeviceDataComponent implements OnInit {
   list = [];
   flag: boolean = false;
   show: boolean = false;
+  display_table: boolean = false;
+  display_chart: boolean = false;
+
+  public lineChartData: Array<any>;
+  public lineChartLabels: Array<any>;
+  public lineChartOptions: any;
+  public lineChartColors: Array<any>;
+  public lineChartLegend: boolean;
+  public lineChartType: string;
+  
 
   constructor(private deviceDataService: DeviceDataService) {
     this.getDeviceList();
+  }
+  drawChart(d:any) {
+    console.log("this is my data in chart : ", d[0].ValueA);
+    var value = [];  
+    var time = [];  
+    for(var i=0;i<d.length;i++){
+      value[i] = d[i].ValueA;
+      time[i] = d[i].CreatedDateTime;
+      console.log("this is value a for " + value[i]);
+      console.log("this is value for time " + time[i]);
+    }
+    this.lineChartData = [
+      { data: value, label: 'Value A' }
+    ];
+    this.lineChartLabels = time;
+    this.lineChartOptions = {
+      responsive: true
+    };
+    this.lineChartColors = [
+      { // grey
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+      }
+    ];
+    this.lineChartLegend = true;
+    this.lineChartType = 'line';
   }
   getDeviceList() {
     this.deviceDataService.getDevices('').subscribe(x => {
@@ -33,6 +74,21 @@ export class DeviceDataComponent implements OnInit {
       this.flag = false;
       if (this.data.length !== 0) {
         this.flag = true;
+        for (var i = 0; i < this.data.length; i++) {
+        var d = new Date(this.data[i].CreatedDateTime);
+        var localTime = d.getTime();
+        this.data[i].CreatedDateTime = new Date((localTime - (new Date().getTimezoneOffset() * 60000))).toLocaleString();
+      }
+        if (this.form.value.displayType === "chart") {
+          this.display_table = false;
+
+          this.drawChart(this.data);
+          this.display_chart = true;
+        } else if (this.form.value.displayType === "table") {
+          this.display_chart = false;
+          this.display_table = true;
+        }
+        console.log(this.data);
       }
     });
   }
@@ -40,6 +96,7 @@ export class DeviceDataComponent implements OnInit {
   ngOnInit() {
     this.form = new FormGroup({
       deviceID: new FormControl('', Validators.compose([Validators.required])),
+      displayType: new FormControl('', Validators.compose([Validators.required])),
     })
   }
 
